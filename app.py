1671cb6c65666c619dcfd3128b9841226b54475c
Ademola Adediran <>
2023-04-04 11:52:20 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def helllo_world():
-  return "Hello, Ademola"
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
